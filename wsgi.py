ba7db7d2ea8dc0f9eaa93134f6844952cffc6879
abhijeet <abhijeetdtu@gmail.com>
2017-10-01 17:35:52 +0530

    Landing html

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask,render_template
+application = Flask(__name__ , template_folder="Frontend/Templates")
 
 @application.route("/")
-def hello():
-    return "Hello World!"
+def Landing():
+    return render_template("Landing.html")
 
 if __name__ == "__main__":
     application.run()
